75f8dc52c87b7792ec89627ae2a5b6aeb1a91965
Elias Dewan <come1stai2amico3@live.it>
2024-07-29 16:44:44 +0100

    added sample image

--- my-app/src/data/uberTaskerScreenInfo/uberDataSample.ts
+++ my-app/src/data/uberTaskerScreenInfo/uberDataSample.ts
@@ -2,68 +2,25 @@
  *  Data from uber app , trip request sample
  */
 export const failedRequest = [
-  { "text": "You\u0027re online" },
-  { "text": "Trip planner" },
-  { "text": "Uber Pro Blue" },
-  { "text": "Elias Roni" },
-  { "text": "4.94" },
-  { "text": "Inbox" },
-  { "text": "7" },
-  { "text": "Refer friends" },
-  { "text": "0" },
-  { "text": "Opportunities" },
-  { "text": "0" },
-  { "text": "Earnings" },
-  { "text": "0" },
-  { "text": "Uber Pro" },
-  { "text": "0" },
-  { "text": "Wallet" },
-  { "text": "0" },
-  { "text": "Account" },
-  { "text": "0" },
-  { "text": "Help" },
-  { "text": "0" },
-  { "text": "Tips \u0026 Info" },
-  { "text": "0" },
   { "text": "UberX" },
-  { "text": "£11.89" },
-  { "text": "4.69" },
-  { "text": "£10.83 + est. holiday pay of £1.06" },
-  { "text": "2 mins (0.2 mi) away" },
+  { "text": "£12.49" },
+  { "text": "4.47" },
+  { "text": "£15.83 + est. holiday pay of £1.08" },
+  { "text": "2 mins (0.3 mi) away" },
   { "text": "UB2, London" },
-  { "text": "7.0 mi trip" },
+  { "text": "6.0 mi trip" },
   { "text": "W3, London" },
   { "text": "Confirm" }
 ]
 
 export const dummyJsonData = [
-  { "text": "Finding trips" },
-  { "text": "Trip planner" },
-  { "text": "Uber Pro Blue" },
-  { "text": "Elias Roni" },
-  { "text": "4.94" },
-  { "text": "Inbox" },
-  { "text": "7" },
-  { "text": "Refer friends" },
-  { "text": "0" },
-  { "text": "Opportunities" },
-  { "text": "0" },
-  { "text": "Earnings" },
-  { "text": "0" },
-  { "text": "Uber Pro" },
-  { "text": "0" },
-  { "text": "Wallet" },
-  { "text": "0" },
-  { "text": "Account" },
-  { "text": "Help" },
-  { "text": "Tips \u0026 Info" },
   { "text": "UberX" },
-  { "text": "£34.61" },
-  { "text": "4.64" }, // 22
-  { "text": "£31.57 + est. holiday pay of £3.04" }, //23
-  { "text": "2 mins (0.1 mi) away" }, //24
+  { "text": "£38.91" },
+  { "text": "4.34" }, // 22
+  { "text": "£36.57 + est. holiday pay of £3.04" }, //23
+  { "text": "2 mins (0.5 mi) away" }, //24
   { "text": "UB2, Southall" },    // 25
-  { "text": "19.7 mi trip" },     // 26
+  { "text": "13.7 mi trip" },     // 26
   { "text": "E14, London" },      // 27
   { "text": "Long trip (60+ min)" },
   { "text": "Confirm" }]
